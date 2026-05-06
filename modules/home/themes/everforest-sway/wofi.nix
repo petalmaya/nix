b0802d08c30dd090204114bfbcd20ab52b185e27
Alice Liddel <sheepos@proton.me>
2026-05-06 21:55:52 +0000

    mmph mmph (new theme)

---
+++ modules/home/themes/everforest-sway/wofi.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  programs.wofi = {
+    enable = true;
+    style = ''
+      window {
+        background-color: #2b3339;
+        color: #d3c6aa;
+        border: 3px solid #a7c080;
+      }
+      #input {
+        background-color: #3a454a;
+        color: #d3c6aa;
+        border: none;
+      }
+      #inner-box {
+        background-color: #2b3339;
+      }
+      #outer-box {
+        background-color: #2b3339;
+      }
+      #text {
+        color: #d3c6aa;
+      }
+      #entry:selected {
+        background-color: #a7c080;
+      }
+      #entry:selected #text {
+        color: #2b3339;
+      }
+    '';
+    settings = {
+      allow_images = true;
+      allow_markup = true;
+      term = "foot";
+      width = 600;
+      height = 400;
+    };
+  };
+}
