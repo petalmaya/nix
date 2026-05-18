7f66ac7882e8a98f330ae1f051a56de2d2043dcb
Alice Liddel <sheepos@proton.me>
2026-05-17 22:45:55 -0600

    Major Cleanup, Conversion to nix devlop boilers, Overall improvemnets.

---
+++ assets/boilers/haskell/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Basic Haskell Development Environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        ghc
+        cabal-install
+        haskell-language-server
+      ];
+    };
+  };
+}
